663c85204401481b9b52517818622f66db8e25d9
kai <kai.kruse@mpi-muenster.mpg.de>
2015-03-18 10:48:03 +0100

    removed dependencies to allow easy upgrade

--- setup.py
+++ setup.py
@@ -4,11 +4,5 @@ setup(
     name='kaic',
     version='0.1.1',
     description=('Vacuum Hi-C data analysis tools.'),
-    packages = find_packages(exclude="test"),
-    install_requires=[
-        'hiclib',
-        'mirnylib',
-        'numpy',
-        'matplotlib'
-    ]
+    packages = find_packages(exclude="test")
 )
