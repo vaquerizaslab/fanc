3f193a11b2b5ddaa45fadf04cd20ddfdf015466a
kai <kai.kruse@mpi-muenster.mpg.de>
2015-03-19 13:46:44 +0100

    Iterative correction script

--- kaic/correcting/iterativeCorrection.py
+++ kaic/correcting/iterativeCorrection.py
@@ -1,37 +1,40 @@
-from hiclib.fragmentHiC import HiCdataset;
 import time;
 import argparse;
 import kaic.genome.genomeTools as gt
+from hiclib import binnedData
 
 def main(args):
     print("Using the following settings");
     for arg, val in args.__dict__.iteritems():
         print arg, " = ", val;
-        
+
     time.sleep(5);
-    
+
     # read in genome object
     genome_db = gt.loadGenomeObject(args.genome)
+
+    BD = binnedData.binnedData(args.resolution, genome_db)
+    BD.simpleLoad(args.input, 'hm')
     
-    TR = HiCdataset("memory", genome=genome_db, inMemory=True);
-    TR.load(args.input);
+
+    BD.iterativeCorrectWithoutSS(force=True)
     
-    if args.byChromosome == False:
-        TR.saveHeatmap(args.output, args.resolution);
-    else:
-        TR.saveByChromosomeHeatmap(args.output, args.resolution);
+    BD.export("hm",args.output)
+    
+    
+
 
 def splitList(thisList):
     return thisList.split(",");
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser();
-    
+
     parser.add_argument(
         'input',
-        help='''Input Hi-C Map file (hdf5 dict)'''
+        help='''Input Hi-C Map file (binned data, hdf5 dict)'''
     );
-    
+
     parser.add_argument(
         'genome',
         help='''Genome object file'''
@@ -41,21 +44,12 @@ if __name__ == '__main__':
         'output',
         help='''Output file'''
     );
-    
+
     parser.add_argument(
         '-res', '--resolution', dest='resolution',
         type=int,
         help='''Cutoff for filtering very large fragments''',
         required=True
     );
-    
-    parser.add_argument(
-        '-bc', '--by-chromosome', dest='bychromosome',
-        action='store_true',
-        help='''Store the heatmap by chromosome rather than as a whole'''
-    );
-    
-    
-    parser.set_defaults(bychromosome=False);
     
     main(parser.parse_args());
