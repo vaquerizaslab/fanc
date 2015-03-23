9c68bfe4a966d3060db5e7c427996f7d28b0cfae
kai <kai.kruse@mpi-muenster.mpg.de>
2015-03-23 10:12:21 +0100

    New distribution

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='kaic',
-    version='0.1.1',
+    version='0.1.2',
     description=('Vacuum Hi-C data analysis tools.'),
     packages = find_packages(exclude="test")
 )
