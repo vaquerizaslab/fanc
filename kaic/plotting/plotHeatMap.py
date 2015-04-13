18e97ac2a9c20aa8dac58bddfa4ba059fed4be20
kai <kai.kruse@mpi-muenster.mpg.de>
2015-04-13 16:30:35 +0200

    Tried to fix plotting function

--- kaic/plotting/plotHeatMap.py
+++ kaic/plotting/plotHeatMap.py
@@ -2,6 +2,7 @@ from hiclib.fragmentHiC import HiCdataset;
 import time;
 import argparse;
 from hiclib import binnedData
+from hiclib import highResBinnedData
 import matplotlib.pyplot as plt
 import kaic.genome.genomeTools as gt
 import numpy as np
@@ -16,23 +17,30 @@ def main(args):
     # read in genome object
     genome_db = gt.loadGenomeObject(args.genome)
     
-    BD = binnedData.binnedData(args.resolution, genome_db)
-    BD.simpleLoad(args.input, 'hm', chromosomeOrder=args.order)
-    #BD.simpleLoad(args.input, 'hm', chromosomeOrder=[11])
+    if args.lr == True:
+        BD = binnedData.binnedData(args.resolution, genome_db)
+        BD.simpleLoad(args.input, 'hm', chromosomeOrder=args.order)
+        #BD.simpleLoad(args.input, 'hm', chromosomeOrder=[11])
+        hm = BD.dataDict['hm']
+    else:
+        BD = highResBinnedData.HiResHiC(args.resolution, genome_db)
+        BD.loadData(args.input)
+        hm = BD.getCombinedMatrix();
+        
     
-    hm = BD.dataDict['hm']
+    
     
     if args.absolute == False:
-        BD.removeZeros()
-        hm = BD.dataDict['hm']
+        #BD.removeZeros()
+        #hm = BD.dataDict['hm']
         nrows = hm.shape[0]
         ncols = hm.shape[1]
         ex = np.sum(hm)/(nrows*ncols)
         print "Expected: ", ex
         hm = np.log2(hm/ex)
-        BD.dataDict['hm'] = hm
-        BD.restoreZeros()
-        hm = BD.dataDict['hm']
+        #BD.dataDict['hm'] = hm
+        #BD.restoreZeros()
+        #hm = BD.dataDict['hm']
     
     fig, ax = plt.subplots()
     myPlot = ax.imshow(hm, interpolation='none',aspect=1,vmin=args.min,vmax=args.max)
@@ -95,6 +103,13 @@ if __name__ == '__main__':
         default='YlGnBu',
         help='''Matplotlib color map name to use'''
     );
+    
+    parser.add_argument(
+        '-l', '--low-res', dest='lr',
+        action='store_true',
+        help='''Use low-resolution analysis'''
+    );
+    parser.set_defaults(lr=False);
     
     parser.set_defaults(absolute=False);
     
