4e38577609929c1ffbb0f4f375d455d31809f15e
kai <kai.kruse@mpi-muenster.mpg.de>
2015-05-13 15:49:23 +0200

    Remove sparse rows until balancing works

--- kaic/tools/matrix.py
+++ kaic/tools/matrix.py
@@ -4,6 +4,8 @@ Created on May 11, 2015
 @author: kkruse1
 '''
 
+import numpy as np
+
 def readMatrixFromFile(file_name, delim="\t"):
     with open(file_name, 'r') as f:
         M = [ map(float,line.split(delim)) for line in f ]
@@ -32,3 +34,25 @@ def writeMatrixToFile(M,file_name,delim="\t",row_names=None,col_names=None):
                 o.write("%.6E%s" + (M[i,j],delim))
 
     print "Done writing to file."
+
+def removeSparseRows(M,cutoff=None):
+    s = np.sum(M,0)
+    
+    if cutoff == None:
+        cutoff = min(s)
+    
+    idxs = np.where(s <= cutoff)[0]
+    A = np.delete(M,idxs,0)
+    A = np.delete(A,idxs,1)
+    
+    return A, idxs
+    
+    
+def restoreSparseRows(M,idxs,rows=None):
+    idxsn = idxs.copy()
+    for i in range(0,len(idxs)):
+        idxsn = idxs[i]-i
+    
+    A = np.insert(M,idxsn,0,axis=0)
+    A = np.insert(A,idxsn,0,axis=1)
+    return A
