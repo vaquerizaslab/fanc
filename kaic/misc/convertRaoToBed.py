5d0b8e5ad1a2af774f6dcd48d84e0d63f7f0792f
kai <kai.kruse@mpi-muenster.mpg.de>
2015-05-21 12:23:32 +0200

    Rao to Bed (properly)

--- kaic/misc/convertRaoToBed.py
+++ kaic/misc/convertRaoToBed.py
@@ -31,12 +31,9 @@ if __name__ == '__main__':
     with open(in_file, 'r') as f:
         with open(out_file, 'w') as o:
             header = f.readline().rstrip().split("\t")
-            header[0] = "chrom1"
-            header[1] = "start1"
-            header[2] = "end1"
-            header[3] = "chrom2"
-            header[4] = "start2"
-            header[5] = "end2"
+            header[0] = "chrom"
+            header[1] = "start"
+            header[2] = "end"
             o.write("\t".join(header) + "\n")
             
             line = f.readline()
