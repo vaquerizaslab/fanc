7e19024567b621a86ccca28ca394ef9d3463d6b8
kai <kai.kruse@mpi-muenster.mpg.de>
2015-05-28 14:20:37 +0200

    file check and line count

--- kaic/analysis/multi_sample_hic.py
+++ kaic/analysis/multi_sample_hic.py
@@ -3,6 +3,8 @@
 import argparse
 import gridmap
 import logging
+import os.path
+from kaic.tools.files import get_number_of_lines
 logging.basicConfig(level=logging.DEBUG)
 
 
@@ -17,7 +19,7 @@ if __name__ == '__main__':
     parser.add_argument(
         'input',
         nargs='+',
-        help='''FASTQ input files'''
+        help='''FASTQ input files basenames (without _*.fastq)'''
     );
     
     parser.add_argument(
@@ -41,4 +43,29 @@ if __name__ == '__main__':
     
     args = parser.parse_args()
     
-    print args
+    
+    
+    # step one:
+    # reconstruct paired FASTQ file names
+    pairs = []
+    n_lines = []
+    n_sum = 0
+    for basename in args.input:
+        file_name1 = basename + "_1.fastq"
+        file_name2 = basename + "_2.fastq"
+        
+        if not os.path.isfile(file_name1):
+            raise IOError("File " + file_name1 + " not found")
+        if not os.path.isfile(file_name2):
+            raise IOError("File " + file_name2 + " not found")
+        
+        pairs.append([file_name1, file_name2])
+        n = get_number_of_lines(file_name1)
+        n_sum += n
+        n_lines.append(n)
+    
+    n_lines_ratios = []
+    for i in range(0,len(n_lines)):
+        n_lines_ratios[i] = n_lines[i]/n_sum
+        
+    print n_lines_ratios
