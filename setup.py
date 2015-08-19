fe1c9efd14a7f5007b82e1bd119498e9afb64869
kai <kai.kruse@mpi-muenster.mpg.de>
2015-08-19 15:45:59 +0200

    Version bump to 0.3 and syntax change for ignoring packages

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='kaic',
-    version='0.2.0',
+    version='0.3.0',
     description=('Vacuum Hi-C data analysis tools.'),
-    packages = find_packages(exclude="test")
+    packages = find_packages(exclude=["test"])
 )
