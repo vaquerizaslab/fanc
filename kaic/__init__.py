a59dc05437f65dc5ac03d8674a5a8f9806bf049c
kai <kai.kruse@mpi-muenster.mpg.de>
2015-08-28 11:18:45 +0200

    Sample Hi-C for convenience

--- kaic/__init__.py
+++ kaic/__init__.py
@@ -15,3 +15,28 @@ logging.basicConfig(level=logging.INFO)
 from kaic.data.genomic import HicBasic, HicNode, HicEdge, Genome, Chromosome
 from kaic.data.general import Table 
 from kaic.construct.seq import ReadPairs
+
+def sample_hic():
+        hic = HicBasic()
+        
+        # add some nodes (120 to be exact)
+        nodes = []
+        for i in range(1,5000,1000):
+            nodes.append(HicNode(chromosome="chr1",start=i,end=i+1000-1))
+        for i in range(1,3000,1000):
+            nodes.append(HicNode(chromosome="chr2",start=i,end=i+1000-1))
+        for i in range(1,2000,500):
+            nodes.append(HicNode(chromosome="chr3",start=i,end=i+1000-1))
+        hic.add_nodes(nodes)
+        
+        # add some edges with increasing weight for testing
+        edges = []
+        weight = 1
+        for i in range(0,len(nodes)):
+            for j in range(i,len(nodes)):
+                edges.append(HicEdge(source=i,sink=j,weight=weight))
+                weight += 1
+
+        hic.add_edges(edges)
+        
+        return hic
