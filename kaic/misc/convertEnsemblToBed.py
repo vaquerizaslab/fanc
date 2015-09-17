f5c378336f012f76065f284c65ffbb982621f637
Kai <kkruse@mpi-muenster.mpg.de>
2015-09-17 10:34:07 +0200

    Ensembl exported file reader

--- kaic/misc/convertEnsemblToBed.py
+++ kaic/misc/convertEnsemblToBed.py
@@ -45,13 +45,14 @@ if __name__ == '__main__':
                 file_name = "_".join(previous_line.split())
                 o = open(out_folder + '/' + file_name + '.bed', 'w')
                 if previous_line.lower().startswith('gene'):
-                    out_header = 'chrom\tstart\tend\tscore\tstrand\tgene'
+                    mode = 3
+                    out_header = 'chrom\tstart\tend\tgene\tscore\tstrand\ttype'
                 else:
                     out_header = 'chrom\tstart\tend\tscore\tstrand\tname'
-                ignore = set([0,1,2,3])
+                ignore = set([0,1,2])
                 for idx, field in enumerate(header):
                     if idx not in ignore and field.lower() != 'name':
-                        out_header += "\t%s" % field
+                        out_header += "\t%s" % ''.join(field.split())
                 o.write(out_header+ '\n')
             elif line == '':
                 o.close()
@@ -89,18 +90,35 @@ if __name__ == '__main__':
                     end = fields[2]
                     score = 1
                     strand = '+'
-                    name = fields[3]
                     
                     out_line = "%s\t%s\t%s\t%s\t%s" % (chromosome, start, end, score, strand)
                     
                     end_line = ''
-                    ignore = set([0,1,2,3])
+                    ignore = set([0,1,2])
                     for idx, field in enumerate(fields):
                         if header[idx].lower() == 'name':
                             out_line += '\t%s' % field
                         elif idx not in ignore:
                             end_line += "\t%s" % field
                     
+                    o.write(out_line + end_line + "\n")
+                elif mode == 3:
+                    chromosome = fields[0]
+                    start = fields[1]
+                    end = fields[2]
+                    score = 1
+                    strand = 1
+
+                    out_line = "%s\t%s\t%s" % (chromosome, start, end)
+
+                    end_line = '\t%s\t%s\texon' % (score, strand)
+                    ignore = set([0,1,2])
+                    for idx, field in enumerate(fields):
+                        if header[idx].lower() == 'name':
+                            out_line += '\t%s' % field
+                        elif idx not in ignore:
+                            end_line += "\t%s" % field
+
                     o.write(out_line + end_line + "\n")
             previous_line = line
         
