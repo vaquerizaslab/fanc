3e89a46e043b9aac62c00b6e98d71c34799440a6
kai <kai.kruse@mpi-muenster.mpg.de>
2015-10-01 10:07:54 +0200

    HiC-Matrix plot

--- kaic/plotting/plot_genomic_data.py
+++ kaic/plotting/plot_genomic_data.py
@@ -7,6 +7,8 @@ Created on May 20, 2015
 from rpy2.robjects import pandas2ri as p2r
 from rpy2.robjects.packages import importr
 import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
 
 def open_graphics_file(file_name, width=None, height=None):
     p2r.activate()
@@ -270,6 +272,42 @@ class HiCPlot(object):
         if output:
             close_graphics_file()
 
+class HicMatrixPlot(object):
+    def __init__(self, hic, chrom=None, start=None, end=None,
+                 zrange=[5,40], colormap=None):
+        self.hic = hic
+        self.chrom = chrom
+        self.start = start
+        self.end = end
+        self.zrange = zrange
+        self.colormap = colormap
+        if self.colormap is None:
+            cdict = {'red': ((0.0, 1.0, 1.0),
+                    (0.28, 0.18, 0.18),
+                    (0.72, 0.78, 0.78),
+                    (1.0, 1.0, 1.0)),
+            'green': ((0.0, 1.0, 1.0),
+                    (0.36, 0.05, 0.05),
+                    (0.49, 0.12, 0.12),
+                    (1.0, 1.0, 1.0)),
+            'blue': ((0.0, 1.0, 1.0),
+                    (0.26, 0.62, 0.62),
+                    (0.37, 0.5, 0.5),
+                    (0.77, 0.2, 0.2),
+                    (0.92, 0.64, 0.64),
+                    (1.0, 1.0, 1.0))
+            }
+            self.colormap = matplotlib.colors.LinearSegmentedColormap("Sexton colormap", cdict, 256)
+    
+    def show(self, output=None):
+        hm = self.hic['%s:%d-%d' % (self.chrom, self.start, self.end),
+                      '%s:%d-%d' % (self.chrom, self.start, self.end)]
+        
+        _, ax = plt.subplots()
+        myPlot = ax.imshow(hm, interpolation='none',aspect=1,vmin=self.zrange[0],vmax=self.zrange[1])
+        myPlot.set_cmap(self.colormap)
+        
+        plt.show()
 
 class BedPlot(object):
     def __init__(self, bed, chrom=None, start=None, end=None,
