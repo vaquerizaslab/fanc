6308ae00ca03a4f3f9d7f77fcc25df418eb449b2
kai <kai.kruse@mpi-muenster.mpg.de>
2015-10-12 14:14:05 +0200

    Same bug fix in newer plotting version

--- kaic/construct/seq.py
+++ kaic/construct/seq.py
@@ -1480,8 +1480,8 @@ class FragmentMappedReadPairs(Maskable, MetaContainer, RegionsTable, FileBased):
         logging.info("Types sum: %d" % sum(types))
         logging.info("Gaps len: %d" % len(gaps))
         logging.info("Types len: %d" % len(types))
-        logging.info("Gaps mean: %d" % sum(gaps)/len(gaps))
-        logging.info("Types mean: %d" % sum(types)/len(types))
+        logging.info("Gaps mean: %.2f" % (sum(gaps)/len(gaps)))
+        logging.info("Types mean: %.2f" % (sum(types)/len(types)))
         
         # calculate ratios
         x = []
