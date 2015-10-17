6e4426832072cb6544f84cfad38a5f9ad224d55a
kai <kai.kruse@mpi-muenster.mpg.de>
2015-10-17 09:20:26 +0200

    Removed pesky rpy2 dependency (I think)

--- kaic/__init__.py
+++ kaic/__init__.py
@@ -12,9 +12,9 @@ Provides
 import logging
 logging.basicConfig(level=logging.INFO)
 
-from kaic.data.genomic import HicBasic, HicNode, HicEdge, Genome, Chromosome, Bed
+from kaic.data.genomic import HicBasic, HicNode, HicEdge, Genome, Chromosome, BedImproved
 from kaic.data.general import Table 
-from kaic.construct.seq import ReadPairs
+from kaic.construct.seq import Reads, FragmentMappedReadPairs
 #from kaic.plotting.plot_genomic_data import HiCPlot, BedPlot, GenomicDataPlot, GenePlot
 
 def sample_hic():
