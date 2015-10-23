12ad70f16dee4c4668c544c25bf1029c0a58408c
kai <kai.kruse@mpi-muenster.mpg.de>
2015-10-23 13:59:27 +0200

    Plotting requirements

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='kaic',
     version='0.4.0',
-    description=('Hi-C data analysis tools.'),
+    description='Hi-C data analysis tools.',
     packages=find_packages(exclude=["test"]),
     install_requires=[
         'tables>=3.2.2',
@@ -12,5 +12,8 @@ setup(
         'pandas',
         'biopython',
         'numpy'
-    ]
+    ],
+    extras_require={
+        'plotting':  ["seaborn"]
+    }
 )
