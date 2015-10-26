deb933181b2b4d46485619f3e077fc8507e4d18c
kai <kai.kruse@mpi-muenster.mpg.de>
2015-10-26 09:59:07 +0100

    Return row pointer (as expected) not numpy.void

--- kaic/data/general.py
+++ kaic/data/general.py
@@ -1647,7 +1647,7 @@ class MaskedTable(t.Table):
                 row = self.iter.next()
                 while row[this._mask_field] > 0:
                     row = self.iter.next()
-                return row.fetch_all_fields()
+                return row
         return VisibleIter()
       
     def __getitem__(self, key):
