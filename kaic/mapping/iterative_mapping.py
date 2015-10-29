7a09107cc4e6cae24a91eb851ce914b0abcb06f2
kai <kai.kruse@mpi-muenster.mpg.de>
2015-10-29 10:17:51 +0100

    New iterative mapping merging

--- kaic/mapping/iterative_mapping.py
+++ kaic/mapping/iterative_mapping.py
@@ -1,15 +1,17 @@
 from hiclib import mapping
-import os
 import logging
 import glob
+import shutil
+import tempfile
+import subprocess
+import os
 
 
 def iterative_mapping(input_files, output_files,
                       bowtie_index, bowtie=None, 
                       min_length=25, step_size=2, 
                       threads=4, tmp_dir="/tmp", 
-                      quality=30, do_join=True,
-                      do_filter=False, do_clean=True,
+                      do_join=True, do_clean=True,
                       bowtie_options='--very-sensitive --no-unal'):
     if not len(input_files) == len(output_files):
         raise ValueError("Input and output arguments must have the same number of elements!")
@@ -40,19 +42,33 @@ def iterative_mapping(input_files, output_files,
             logging.info("Joining output files into %s" % (output_files[i]))
 
             sam_files = glob.glob("%s.*" % output_files[i])
+            output_path = output_files[i]
+            shutil.copy(sam_files.pop(0), output_path)
 
+            # join two files and copy back to output_path
+            for sam_file in sam_files:
+                tmp_file = tempfile.NamedTemporaryFile(delete=False)
+                tmp_file.close()
+                tmp_path = tmp_file.name
 
-            if do_filter:
-                logging.info("Filtering...")
-                # merge and filter output sam files for:
-                # - mappability
-                # - mapping quality
-                # - uniqueness
-                os.popen('{ head -n 20000 -q %s.* | grep "^@[HD|SQ]" | sort | uniq & cat %s.* | awk \'{ duplicate=0; for(i=12; i<=NF; ++i) if($i ~ /^XS/) duplicate=1; if(duplicate == 0 && $1 !~ /^@/ && $2 != 4 && $5 > %d) print $0; }\' | sort -k1,1 -k5,5rn | awk -v last="" \'{ if(last != $1) print $0; last=$1}\'; } > %s' % (output_files[i], output_files[i], quality, output_files[i]));
-            else:
-                os.popen('{ head -n 20000 -q %s.* | grep "^@[HD|SQ]" | sort | uniq & cat %s.* | awk \'!/^@/\' | sort -k1,1 -k5,5rn | awk -v last="" \'{ if(last != $1) print $0; last=$1}\'; } > %s' % (output_files[i],output_files[i],output_files[i]));
-            # remove partial sam files
-            
-            if do_clean:
-                os.popen('rm -f %s.*' % (output_files[i]));
+                try:
+                    subprocess.check_call('head -n 20000 -q %s %s | ' % (output_path, sam_file) +
+                                          'grep "^@[HD|SQ]" | sort | uniq > %s' % tmp_path)
+                except subprocess.CalledProcessError:
+                    logging.error("Could not join %s into file!" % sam_file)
+                    os.unlink(tmp_path)
+                    continue
 
+                try:
+                    subprocess.check_call('cat %s %s | awk \'!/^@/\' | ' % (output_path, sam_file) +
+                                          'sort -k1,1 -k5,5rn | ' +
+                                          'awk -v last="" \'{ if(last != $1) print $0; last=$1}\' >> %s' % tmp_path)
+                except subprocess.CalledProcessError:
+                    logging.error("Could not join %s into file!" % sam_file)
+                    os.unlink(tmp_path)
+                    continue
+
+                if do_clean:
+                    os.unlink(sam_file)
+                os.unlink(output_path)
+                shutil.move(tmp_path, output_path)
