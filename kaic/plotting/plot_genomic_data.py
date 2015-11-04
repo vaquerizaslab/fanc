5ec115ca2c20db5cb33fe2c3411440e984989586
kai <kai.kruse@mpi-muenster.mpg.de>
2015-11-04 16:54:25 +0100

    MA plot in 2D and 3D with highlights

--- kaic/plotting/plot_genomic_data.py
+++ kaic/plotting/plot_genomic_data.py
@@ -1,8 +1,30 @@
+from __future__ import division
 import seaborn as sns
 import pandas
 import numpy as np
 import kaic.data.general as general
 import kaic.data.genomic as genomic
+from mpl_toolkits.mplot3d import Axes3D
+# import os.path
+
+
+def _prepare_backend(output):
+    if output is not None:
+        old_backend = sns.plt.get_backend()
+        # extension = os.path.splitext(output)[1]
+        sns.plt.switch_backend('pdf')
+        sns.plt.ioff()
+        return old_backend
+    return None
+
+def _plot_figure(figure, output, old_backend):
+    if output is not None:
+        figure.figure.savefig(output)
+        sns.plt.close(figure.figure)
+        sns.plt.ion()
+        sns.plt.switch_backend(old_backend)
+    else:
+        sns.plt.show()
 
 
 def hic_contact_plot_linear(hic, regions, output=None, window_size=1000000):
@@ -185,38 +207,126 @@ def hic_correlation_plot(hic1, hic2, output=None, include_zeros=False, colormap=
         sns.plt.show()
 
 
-def hic_ma_plot(hic1, hic2, output=None, key=slice(0, None, None),
-                colormap='RdBu_r', log=True):
+def hic_ma_plot(hic1, hic2, output=None, highlights=None, key=slice(0, None, None),
+                colormap='RdBu_r', log_x=True, log_y=True, inter_chromosomal=True,
+                size_factor_correct=True, plot_3d=False):
 
     hm1 = hic1[key, key]
     hm2 = hic2[key, key]
-    if log:
-        hm = np.log2(hm1/hm2)
+
+    # comparison correction factor
+    if size_factor_correct:
+        ccf = np.sum(hm1)/np.sum(hm2)
+        hm2 *= ccf
+
+    # generate a highlights dictionary
+    highlights_dict = None
+    if highlights is not None:
+        highlights_dict = dict()
+        for highlight in highlights:
+            if highlight.chromosome not in highlights_dict:
+                highlights_dict[highlight.chromosome] = []
+            highlights_dict[highlight.chromosome].append(highlight)
+
+    main_region_tuples = []
+    main_contacts_1 = []
+    main_contacts_2 = []
+    main_fold_changes = []
+    main_colors = []
+    highlighted_region_tuples = []
+    highlighted_contacts_1 = []
+    highlighted_contacts_2 = []
+    highlighted_fold_changes = []
+    highlighted_colors = []
+    if len(hm1.shape) == 2:
+        for i in xrange(0, hm1.shape[0]):
+            row_region = hm1.row_regions[i]
+            for j in xrange(i, hm1.shape[1]):
+                col_region = hm1.col_regions[j]
+                if hm1[i, j] == 0 or hm2[i, j] == 0:
+                    continue
+
+                if not inter_chromosomal and row_region.chromosome != col_region.chromosome:
+                    continue
+
+                fold_change = hm1[i, j]/(hm2[i, j])
+                if log_y:
+                    fold_change = np.log2(fold_change)
+
+                c = 'lightgray'
+                if highlights_dict is not None:
+                    overlaps_row = False
+                    overlaps_col = False
+                    if row_region.chromosome in highlights_dict:
+                        for highlight in highlights_dict[row_region.chromosome]:
+                            if row_region.overlaps(highlight):
+                                overlaps_row = True
+                            if col_region.overlaps(highlight):
+                                overlaps_col = True
+                        if row_region.chromosome != col_region.chromosome:
+                            for highlight in highlights_dict[col_region.chromosome]:
+                                if row_region.overlaps(highlight):
+                                    overlaps_row = True
+                                if col_region.overlaps(highlight):
+                                    overlaps_col = True
+
+                    if overlaps_row and overlaps_col:
+                        c = 'orangered'
+                    elif overlaps_col or overlaps_row:
+                        c = 'khaki'
+
+                if c == 'lightgray':
+                    main_contacts_1.append(hm1[i, j])
+                    main_contacts_2.append(hm2[i, j])
+                    main_fold_changes.append(fold_change)
+                    main_region_tuples.append((i, j))
+                    main_colors.append(c)
+                else:
+                    highlighted_contacts_1.append(hm1[i, j])
+                    highlighted_contacts_2.append(hm2[i, j])
+                    highlighted_fold_changes.append(fold_change)
+                    highlighted_region_tuples.append((i, j))
+                    highlighted_colors.append(c)
+
+    old_backend = _prepare_backend(output)
+    scatter = sns.plt.figure()
+
+    if not plot_3d:
+        ax = scatter.add_subplot(111)
+        mean_contacts = (np.array(main_contacts_1)+np.array(main_contacts_2))/2
+        if log_x:
+            mean_contacts = np.log10(mean_contacts)
+        ax.scatter(mean_contacts, main_fold_changes, c=main_colors)
+        if len(highlighted_region_tuples) > 0:
+            h_mean_contacts = (np.array(highlighted_contacts_1)+np.array(highlighted_contacts_2))/2
+            if log_x:
+                h_mean_contacts = np.log10(h_mean_contacts)
+            ax.scatter(h_mean_contacts,
+                       highlighted_fold_changes, c=highlighted_colors)
     else:
-        hm = hm1/hm2
+        ax = scatter.add_subplot(111, projection='3d')
+        if log_x:
+            ax.scatter(np.log10(main_contacts_1), np.log10(main_contacts_2),
+                       main_fold_changes, c=main_colors, alpha=0.3)
+        else:
+            ax.scatter(main_contacts_1, main_contacts_2, main_fold_changes,
+                       c=main_colors, alpha=0.3)
+        if len(highlighted_region_tuples) > 0:
+            if log_x:
+                ax.scatter(np.log10(highlighted_contacts_1), np.log10(highlighted_contacts_2),
+                           highlighted_fold_changes, c=highlighted_colors)
+            else:
+                ax.scatter(highlighted_contacts_1, highlighted_contacts_2,
+                           highlighted_fold_changes, c=highlighted_colors)
 
-    mean_contacts = []
-    fold_changes = []
-    if len(hm.shape) == 2:
-        for i in xrange(0, hm.shape[0]):
-            for j in xrange(i, hm.shape[1]):
-                mean_contact = np.mean((hm1[i, j], hm2[i, j]))
-                fold_change = hm[i, j]
-                mean_contacts.append(mean_contact)
-                fold_changes.append(fold_change)
+    _plot_figure(scatter, output, old_backend)
 
-    old_backend = None
-    if output is not None:
-        old_backend = sns.plt.get_backend()
-        sns.plt.switch_backend('pdf')
-        sns.plt.ioff()
+    return None
 
-    scatter = sns.plt.scatter(mean_contacts, fold_changes)
 
-    if output is not None:
-        scatter.figure.savefig(output)
-        sns.plt.close(scatter.figure)
-        sns.plt.ion()
-        sns.plt.switch_backend(old_backend)
-    else:
-        sns.plt.show()
+def hic_marginals_plot(hic, output=None):
+    marginals = hic.marginals()
+
+    old_backend = _prepare_backend(output)
+    scatter = sns.plt.plot(marginals)
+    _plot_figure(scatter, output, old_backend)
