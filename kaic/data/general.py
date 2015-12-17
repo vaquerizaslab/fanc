2d53668dca6886dfb840ce9da3e2ebeab35ceac4
Alexis GRIMALDI <alexis.grimaldi@gmail.com>
2015-12-17 17:47:10 +0100

    Fixes unnecessary call to super(Maskable)

--- kaic/data/general.py
+++ kaic/data/general.py
@@ -1362,9 +1362,6 @@ class Maskable(object):
                                 pytables file, does not usually need to be 
                                 modified
         """
-        
-        super(Maskable, self).__init__()
-
         # check what we have in data
         mask_file = None
         
