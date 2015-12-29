14edd5630f5eca2305231cfdb9c24147f50802e2
Clemens <clemens.hug@mpi-muenster.mpg.de>
2015-12-29 16:54:39 +0100

    Added first version of analysis module containing functions to calculate insulation indices and contact bands.

---
+++ kaic/analyzing/architecture.py
@@ -0,0 +1,38 @@
+from kaic.data.genomic import Hic
+import numpy as np
+
+def insulation_index(hic, d):
+    chr_bins = hic.chromosome_bins
+    n = len(hic.regions())
+    hic_matrix = hic[:,:]
+    ins_matrix = np.empty(n)
+    for i, r in enumerate(hic.regions()):
+        if i - chr_bins[r.chromosome][0] < d:
+            ins_matrix[i] = np.nan
+            continue
+        if chr_bins[r.chromosome][1] - i < d:
+            ins_matrix[i] = np.nan
+            continue
+        ins_matrix[i] = np.ma.sum(hic_matrix[i: i + d, i - d:i])
+    return ins_matrix
+
+def contact_band(hic, d1, d2, use_oe_ratio=False):
+    chr_bins = hic.chromosome_bins
+    n = len(hic.regions())
+    hic_matrix = hic[:,:]
+    band = np.empty(n)
+    if use_oe_ratio:
+        raise NotImplementedError("oe not implemented yet :(")
+    for i, r in enumerate(hic.regions()):
+        if i - chr_bins[r.chromosome][0] < d2:
+            ins_matrix[i] = np.nan
+            continue
+        if chr_bins[r.chromosome][1] - i < d2:
+            ins_matrix[i] = np.nan
+            continue
+        ins_matrix[i] = np.ma.sum(hic_matrix[i - d2:i - d1, i + d1:i + d2])
+    return ins_matrix
+
+def observed_expected_ratio(hic, per_chromosome=True):
+    pass
+    
