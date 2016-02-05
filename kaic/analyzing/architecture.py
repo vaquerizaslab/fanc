107429c5b58b11a5b5533bc8df8810f5cab74466
Clemens Hug <clemens.hug@mpi-muenster.mpg.de>
2016-02-05 11:39:46 +0100

    Added directionality index calculations. Architecture tracks can now be calculatd from Observed/expected maps

--- kaic/analyzing/architecture.py
+++ kaic/analyzing/architecture.py
@@ -104,32 +104,93 @@ def contact_band(hic, d1, d2, mask_thresh=.5, hic_matrix=None, use_oe_ratio=Fals
     log.info("Skipped {} regions because >{:.1%} of matrix positions were masked".format(skipped, mask_thresh))
     return band
 
-def observed_expected_ratio(hic, hic_matrix=None, per_chromosome=True):
+def directionality_index(hic, d, mask_thresh=.5, correct_sum=True, stat=np.ma.sum):
+    chr_bins = hic.chromosome_bins
+    n = len(hic.regions())
+    log.debug("Fetching matrix")
+    hic_matrix = hic[:,:].masked_matrix
+    e_matrix = expected(hic, hic_matrix=hic_matrix)
+    di = np.empty(n)
+    log.debug("Starting processing")
+    skipped = 0
+    for i, r in enumerate(hic.regions()):
+        if (i - chr_bins[r.chromosome][0] < d or
+            chr_bins[r.chromosome][1] - i < d):
+            di[i] = np.nan
+            continue
+        up_masked = np.sum(hic_matrix.mask[i: i + 1, i - d:i])
+        down_masked = np.sum(hic_matrix.mask[i: i + 1, i + 1:i + d + 1])
+        log.info("Up_ratio {} Down_ratio {}".format(1 - (up_masked/d), 1- (down_masked/d)))
+        if (up_masked > d*mask_thresh or
+            down_masked > d*mask_thresh):
+            # If too close to the edge of chromosome or
+            # if more than half of the entries in this quadrant are masked (unmappable)
+            # exclude it from the analysis
+            skipped += 1
+            di[i] = np.nan
+            continue
+        up_sum = stat(hic_matrix[i: i + 1, i - d:i])
+        down_sum = stat(hic_matrix[i: i + 1, i + 1:i + d + 1])
+        expected_sum = stat(e_matrix[i: i + 1, i + 1:i + d + 1])
+        if correct_sum:
+            up_sum /= 1 - (up_masked/d)
+            down_sum /= 1 - (down_masked/d)
+        di[i] = ((down_sum - up_sum)/abs(down_sum - up_sum))*(((up_sum - expected_sum)**2)/expected_sum + ((down_sum - expected_sum)**2)/expected_sum)
+    log.info("Skipped {} regions because >{:.1%} of matrix positions were masked".format(skipped, mask_thresh))
+    return di
+
+def observed_expected_ratio(hic, hic_matrix=None, per_chromosome=True, stat=np.ma.mean):
     chr_bins = hic.chromosome_bins
     n = len(hic.regions())
     if hic_matrix is None:
         log.debug("Fetching matrix")
-        hic_matrix = hic[:,:]
+        hic_matrix = hic[:,:].masked_matrix
     oe = hic_matrix.copy()
     log.debug("starting processing")
     if per_chromosome:
         for so, eo in chr_bins.itervalues():
             # Correcting intrachromosomal contacts by mean contact count at each diagonal
-            for i in range(n):
-                oe[so:eo, so:eo][kth_diag_indices(eo - so, -i)] /= np.ma.mean(hic_matrix[so:eo, so:eo][kth_diag_indices(eo - so, -i)])
+            for i in range(eo - so):
+                oe[so:eo, so:eo][kth_diag_indices(eo - so, -i)] /= stat(hic_matrix[so:eo, so:eo][kth_diag_indices(eo - so, -i)])
             # Correcting interchromosomal contacts by mean of all contact counts between
             # each set of chromosomes
             for si, ei in chr_bins.itervalues():
                 # Only correct upper triangle
                 if si <= so:
                     continue
-                oe[so:eo, si:ei] /= np.ma.mean(hic_matrix[so:eo, si:ei])
+                oe[so:eo, si:ei] /= stat(hic_matrix[so:eo, si:ei])
     else:
         for i in range(n):
-            oe[kth_diag_indices(n, i)] /= np.ma.mean(hic_matrix.diagonal(i))
+            oe[kth_diag_indices(n, -i)] /= stat(hic_matrix.diagonal(i))
     # Copying upper triangle to lower triangle
     oe[np.tril_indices(n)] = oe.T[np.tril_indices(n)]
     return oe
+
+def expected(hic, hic_matrix=None, per_chromosome=True, stat=np.ma.mean):
+    chr_bins = hic.chromosome_bins
+    n = len(hic.regions())
+    if hic_matrix is None:
+        log.debug("Fetching matrix")
+        hic_matrix = hic[:,:].masked_matrix
+    log.debug("starting processing")
+    expected = np.zeros(hic_matrix.shape)
+    if per_chromosome:
+        for so, eo in chr_bins.itervalues():
+            for i in range(eo - so):
+                expected[so:eo, so:eo][kth_diag_indices(eo - so, -i)] = stat(hic_matrix[so:eo, so:eo][kth_diag_indices(eo - so, -i)])
+            # Correcting interchromosomal contacts by mean of all contact counts between
+            # each set of chromosomes
+            for si, ei in chr_bins.itervalues():
+                # Only correct upper triangle
+                if si <= so:
+                    continue
+                expected[so:eo, si:ei] = stat(hic_matrix[so:eo, si:ei])
+    else:
+        for i in range(n):
+            expected[kth_diag_indices(n, -i)] = stat(hic_matrix.diagonal(-i))
+    # Copying upper triangle to lower triangle
+    expected[np.tril_indices(n)] = expected.T[np.tril_indices(n)]
+    return expected
 
 def create_gtf_from_region_ix(regions, region_ix):
     for s, e in region_ix:
@@ -209,7 +270,7 @@ class PeakCallerDeriv(object):
                 out[i] = func(cur_window[~np.isnan(cur_window)])
             return out
         if abs_thresh:
-            abs_pass = np.where(self._min_mask, self.x_smooth < abs_thresh[0], self.x_smooth > abs_thresh[1])
+            abs_pass = np.where(self._min_mask, self.x_smooth[self._peaks] < abs_thresh[0], self.x_smooth[self._peaks] > abs_thresh[1])
             self._peak_mask = np.logical_and(self._peak_mask, abs_pass)
             log.info("Discarding {}({:.1%}) of total peaks due to absolute value threshold ({})".format(np.sum(~abs_pass), np.sum(~abs_pass)/len(self._peaks), abs_thresh))
         if z_score_thresh:
