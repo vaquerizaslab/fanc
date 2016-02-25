ae179caf94e52280ce8aaf00e56ad64895dadb53
kai <kai.kruse@mpi-muenster.mpg.de>
2016-02-25 12:26:56 +0100

    Complete PCA with flexible pair selection

--- kaic/architecture/pca.py
+++ kaic/architecture/pca.py
@@ -1,9 +1,10 @@
 from __future__ import division
-from kaic.data.genomic import Edge
-from kaic.architecture.hic_architecture import MatrixArchitecturalRegionFeature, \
-    BackgroundLigationFilter, ExpectedObservedEnrichmentFilter, ZeroWeightFilter, \
+from kaic.architecture.hic_architecture import BackgroundLigationFilter, \
+    ExpectedObservedEnrichmentFilter, ZeroWeightFilter, \
     ExpectedObservedCollectionFilter, BackgroundLigationCollectionFilter, \
     HicEdgeCollection
+from sklearn.decomposition import PCA
+from abc import ABCMeta, abstractmethod
 import tables as t
 import numpy as np
 import tempfile
@@ -48,7 +49,46 @@ class HicCollectionWeightMeanVariance(HicEdgeCollection):
         self._edges.cols.var.create_csindex()
 
 
-def do_pca(hics, tmpdir=None, sample_size=10000):
+class PairSelection(object):
+    __metaclass__ = ABCMeta
+
+    def __init__(self):
+        self.collection = None
+        pass
+
+    def set_collection(self, collection):
+        self.collection = collection
+
+    @abstractmethod
+    def pair_selection(self, **kwargs):
+        pass
+
+
+class LargestVariancePairSelection(PairSelection):
+    def __init__(self, sample_size=100000, lazy=False):
+        PairSelection.__init__(self)
+        self.sample_size = sample_size
+        self.lazy = lazy
+
+    def pair_selection(self, sample_size=None, lazy=None):
+        if lazy is None:
+            lazy = self.lazy
+        if sample_size is None:
+            sample_size = self.sample_size
+
+        for j, edge in enumerate(self.collection.edges_sorted('var', reverse=True, lazy=lazy)):
+            yield edge
+
+            if j >= sample_size:
+                # raise StopIteration
+                break
+
+
+def do_pca(hics, pair_selection=None, tmpdir=None, eo_cutoff=0.0, bg_cutoff=1.0,
+           log=True, **kwargs):
+    if pair_selection is None:
+        pair_selection = LargestVariancePairSelection()
+
     if tmpdir is not None:
         tmpdir = tempfile.mkdtemp(dir=os.path.expanduser(tmpdir))
     else:
@@ -60,41 +100,29 @@ def do_pca(hics, tmpdir=None, sample_size=10000):
     coll = HicCollectionWeightMeanVariance(hics, file_name=tmpdir + 'coll.m')
     coll.calculate()
 
-    eof = ExpectedObservedCollectionFilter(coll)
-    bgf = BackgroundLigationCollectionFilter(coll)
-    coll.filter(eof, queue=True)
-    coll.filter(bgf, queue=True)
+    if eo_cutoff != 0.0:
+        eof = ExpectedObservedCollectionFilter(coll)
+        coll.filter(eof, queue=True)
+
+    if bg_cutoff != 1.0:
+        bgf = BackgroundLigationCollectionFilter(coll)
+        coll.filter(bgf, queue=True)
     coll.run_queued_filters()
 
-    # filter "uninteresting" contacts
-    logging.info("Filtering uninteresting contacts")
-    for hic in hics_copy:
-        print len(hic.edges)
-        blf = BackgroundLigationFilter(hic, fold_change=5)
-        eof = ExpectedObservedEnrichmentFilter(hic, fold_change=1.5)
-        hic.filter(blf, queue=True)
-        hic.filter(eof, queue=True)
-        hic.run_queued_filters()
-        print len(hic.edges)
-
-    # join into one object
-    logging.info("Joining objects")
-    coll = HicWeightMeanVariance(hics, file_name=tmpdir + 'coll.m')
-    zwf = ZeroWeightFilter()
-    logging.info("Filtering zero-weights")
-    coll.filter(zwf)
-
     values = []
-    current = 0
-    for edge in coll.edges_sorted('var', reverse=True, lazy=True):
-        if np.isnan(edge.var):
-            continue
-
-        current += 1
-        if current >= sample_size:
-            break
-
+    for edge in pair_selection.pair_selection(**kwargs):
         weights = []
-        for i in xrange(0, len(hics)):
-            weights.append(edge['weight_' + str(i)])
+        for i in xrange(len(hics)):
+            weights.append(getattr(edge, 'weight_' + str(i)))
         values.append(weights)
+
+    if log:
+        y = np.array(values)
+    else:
+        y = np.log(np.array(values))
+
+    pca = PCA()
+    pca_res = pca.fit_transform(y.T)
+    logging.info("Variance explained: %s" % str(pca.explained_variance_ratio_))
+
+    return pca, pca_res
