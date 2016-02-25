71771218eba61ba310f6597b1d2bf19387d3a002
kai <kai.kruse@mpi-muenster.mpg.de>
2016-02-25 11:48:10 +0100

    Hic collection object to hic architectural features. Some PCA updates (WIP)

--- kaic/architecture/hic_architecture.py
+++ kaic/architecture/hic_architecture.py
@@ -2,10 +2,12 @@ from __future__ import division
 from kaic.architecture.architecture import TableArchitecturalFeature, calculateondemand
 from kaic.architecture.genome_architecture import MatrixArchitecturalRegionFeature, VectorArchitecturalRegionFeature, \
     MatrixArchitecturalRegionFeatureFilter
-from kaic.data.genomic import GenomicRegion, HicEdgeFilter
+from kaic.data.genomic import GenomicRegion, HicEdgeFilter, Edge
 from collections import defaultdict
 import numpy as np
 import tables as t
+import logging
+logging.basicConfig(level=logging.INFO)
 
 
 class HicArchitecture(object):
@@ -14,6 +16,97 @@ class HicArchitecture(object):
 
     def expected_contacts(self, per_chromosome=False):
         pass
+
+
+class HicEdgeCollection(MatrixArchitecturalRegionFeature):
+    def __init__(self, hics=None, additional_fields=None, file_name=None, mode='a', tmpdir=None,
+                 only_intra_chromosomal=False):
+        if not isinstance(hics, str) and hics is not None:
+
+            if additional_fields is not None:
+                if not isinstance(additional_fields, dict) and issubclass(additional_fields, t.IsDescription):
+                    # IsDescription subclass case
+                    additional_fields = additional_fields.columns
+            else:
+                additional_fields = dict()
+
+            original_fields = additional_fields.copy()
+
+            self.shared_base_field_names = []
+            for i, hic in enumerate(hics):
+                field_descriptions = hic._edges.coldescrs
+                for name, description in field_descriptions.iteritems():
+
+                    if name.startswith("_") or name in {'source', 'sink'}:
+                        continue
+
+                    for j in xrange(len(hics)):
+                        new_name = "%s_%d" % (name, j)
+                        if new_name in original_fields:
+                            raise ValueError("%s already in hic object, please choose different name." % new_name)
+                        if new_name in additional_fields:
+                            continue
+
+                        if name not in self.shared_base_field_names:
+                            self.shared_base_field_names.append(name)
+
+                        description_args = {'dflt': description.dflt,
+                                            'pos': len(additional_fields)}
+                        if description.__class__ == t.description.StringCol:
+                            description_args['itemsize'] = description.itemsize
+
+                        additional_fields[new_name] = description.__class__(**description_args)
+            MatrixArchitecturalRegionFeature.__init__(self, file_name=file_name, mode=mode,
+                                                      data_fields=additional_fields,
+                                                      regions=hics[0].regions, tmpdir=tmpdir)
+        elif (hics is None and file_name is not None) or file_name is None:
+            if hics is not None and file_name is None:
+                file_name = hics
+            MatrixArchitecturalRegionFeature.__init__(self, file_name=file_name, mode=mode, tmpdir=tmpdir)
+            self._calculated = True
+
+        self.hics = hics
+        self.only_intra_chromosomal = only_intra_chromosomal
+
+    def _calculate(self, *args, **kwargs):
+        chromosomes = self.hics[0].chromosomes()
+
+        # step 1: combine all hic objects into one
+        #         and calculate variance
+        for chr_i, chromosome1 in enumerate(chromosomes):
+            for chr_j in xrange(chr_i, len(chromosomes)):
+                chromosome2 = chromosomes[chr_j]
+
+                if self.only_intra_chromosomal and chromosome1 != chromosome2:
+                    continue
+
+                logging.info("Processing chromosomes %s-%s" % (chromosome1, chromosome2))
+
+                edges = dict()
+                for i, hic in enumerate(self.hics):
+                    logging.info("Processing Hic %d/%d" % (i, len(self.hics)))
+
+                    for edge in hic.edge_subset(key=(chromosome1, chromosome2), lazy=True):
+                        key = (edge.source, edge.sink)
+                        if key not in edges:
+                            edges[key] = {}
+                        for field in self.shared_base_field_names:
+                            if field not in edges[key]:
+                                edges[key][field] = [None] * len(self.hics)
+                            edges[key][field][i] = getattr(edge, field, None)
+
+                for key, d in edges.iteritems():
+                    for field, values in d.iteritems():
+                        source = key[0]
+                        sink = key[1]
+
+                        d = dict()
+                        for i, value in enumerate(values):
+                            d["%s_%d" % (field, i)] = value
+
+                        self.add_edge(Edge(source=source, sink=sink, **d), flush=False)
+
+        self.flush()
 
 
 class ExpectedContacts(TableArchitecturalFeature):
--- kaic/architecture/pca.py
+++ kaic/architecture/pca.py
@@ -1,98 +1,100 @@
-from kaic.data.genomic import MatrixArchitecturalRegionFeature, Edge
+from __future__ import division
+from kaic.data.genomic import Edge
+from kaic.architecture.hic_architecture import MatrixArchitecturalRegionFeature, \
+    BackgroundLigationFilter, ExpectedObservedEnrichmentFilter, ZeroWeightFilter, \
+    ExpectedObservedCollectionFilter, BackgroundLigationCollectionFilter, \
+    HicEdgeCollection
 import tables as t
 import numpy as np
+import tempfile
+import os.path
+import logging
+logging.basicConfig(level=logging.INFO)
 
 
-class HicEdgeCollection(MatrixArchitecturalRegionFeature):
-    def __init__(self, hics, additional_fields=None, file_name=None, mode='a', tmpdir=None):
-        if additional_fields is not None:
-            if not isinstance(additional_fields, dict) and issubclass(additional_fields, t.IsDescription):
-                # IsDescription subclass case
-                additional_fields = additional_fields.columns
-        else:
-            additional_fields = dict()
-
-        original_fields = additional_fields.copy()
-
-        self.shared_base_field_names = []
-        for i, hic in enumerate(hics):
-            field_descriptions = hic._edges.coldescrs
-            for name, description in field_descriptions.iteritems():
-
-                if name.startswith("_") or name in {'source', 'sink'}:
-                    continue
-
-                for j in xrange(len(hics)):
-                    new_name = "%s_%d" % (name, j)
-                    if new_name in original_fields:
-                        raise ValueError("%s already in hic object, please choose different name." % new_name)
-                    if new_name in additional_fields:
-                        continue
-
-                    if name not in self.shared_base_field_names:
-                        self.shared_base_field_names.append(name)
-
-                    description_args = {'dflt': description.dflt,
-                                        'pos': len(additional_fields)}
-                    if description.__class__ == t.description.StringCol:
-                        description_args['itemsize'] = description.itemsize
-
-                    additional_fields[new_name] = description.__class__(**description_args)
-
-        MatrixArchitecturalRegionFeature.__init__(self, file_name=file_name, mode=mode,
-                                                  data_fields=additional_fields,
-                                                  regions=hics[0].regions, tmpdir=tmpdir)
-
-        self.hics = hics
-
-    def _calculate(self, *args, **kwargs):
-        chromosomes = self.hics[0].chromosomes()
-
-        # step 1: combine all hic objects into one
-        #         and calculate variance
-        for chr_i, chromosome1 in enumerate(chromosomes):
-            for chr_j in xrange(chr_i, len(chromosomes)):
-                chromosome2 = chromosomes[chr_j]
-
-                edges = dict()
-                for i, hic in enumerate(self.hics):
-                    for edge in hic.edge_subset(key=(chromosome1, chromosome2), lazy=False):
-                        key = (edge.source, edge.sink)
-                        if key not in edges:
-                            edges[key] = {}
-                        for field in self.shared_base_field_names:
-                            if field not in edges[key]:
-                                edges[key][field] = [None] * len(self.hics)
-                            edges[key][field][i] = getattr(edge, field, None)
-
-                for key, d in edges.iteritems():
-                    for field, values in d.iteritems():
-                        source = key[0]
-                        sink = key[1]
-
-                        d = dict()
-                        for i, value in enumerate(values):
-                            d["%s_%d" % (field, i)] = value
-
-                        self.add_edge(Edge(source=source, sink=sink, **d), flush=False)
-
-        self.flush()
-
-
-class HicWeightVariance(HicEdgeCollection):
-    def __init__(self, hics, file_name=None, mode='a', tmpdir=None):
-        additional_fields = {'var': t.Float32Col()}
+class HicCollectionWeightMeanVariance(HicEdgeCollection):
+    def __init__(self, hics=None, file_name=None, mode='a', tmpdir=None,
+                 only_intra_chromosomal=False, scale_libraries=False):
+        additional_fields = {'var': t.Float32Col(pos=0), 'mean': t.Float32Col(pos=1)}
         HicEdgeCollection.__init__(self, hics, additional_fields=additional_fields, file_name=file_name,
-                                   mode=mode, tmpdir=tmpdir)
+                                   mode=mode, tmpdir=tmpdir, only_intra_chromosomal=only_intra_chromosomal)
+        self.scale_libraries = scale_libraries
 
     def _calculate(self, *args, **kwargs):
         HicEdgeCollection._calculate(self, *args, **kwargs)
 
+        weight_sums = np.zeros(len(self.hics))
         for edge in self.edges(lazy=True):
             weights = np.zeros(len(self.hics))
             for i in xrange(len(self.hics)):
-                weights[i] = getattr(edge, 'weight_' + str(i), 0.0)
+                weight = getattr(edge, 'weight_' + str(i), 0.0)
+                if np.isnan(weight):
+                    weight = 0.0
+                    setattr(edge, 'weight_' + str(i), 0.0)
+                weights[i] = weight
+                weight_sums[i] += weight
             edge.var = np.var(weights)
+            edge.mean = np.mean(weights)
+        self.flush()
+
+        if self.scale_libraries:
+            weight_ratios = weight_sums/weight_sums[0]
+            for edge in self.edges(lazy=True):
+                for i in xrange(len(self.hics)):
+                    weight = getattr(edge, 'weight_' + str(i))
+                    setattr(edge, 'weight_' + str(i), weight/weight_ratios[i])
         self.flush()
 
         self._edges.cols.var.create_csindex()
+
+
+def do_pca(hics, tmpdir=None, sample_size=10000):
+    if tmpdir is not None:
+        tmpdir = tempfile.mkdtemp(dir=os.path.expanduser(tmpdir))
+    else:
+        tmpdir = tempfile.mkdtemp()
+    if not tmpdir.endswith('/'):
+        tmpdir += '/'
+
+    logging.info("Joining objects")
+    coll = HicCollectionWeightMeanVariance(hics, file_name=tmpdir + 'coll.m')
+    coll.calculate()
+
+    eof = ExpectedObservedCollectionFilter(coll)
+    bgf = BackgroundLigationCollectionFilter(coll)
+    coll.filter(eof, queue=True)
+    coll.filter(bgf, queue=True)
+    coll.run_queued_filters()
+
+    # filter "uninteresting" contacts
+    logging.info("Filtering uninteresting contacts")
+    for hic in hics_copy:
+        print len(hic.edges)
+        blf = BackgroundLigationFilter(hic, fold_change=5)
+        eof = ExpectedObservedEnrichmentFilter(hic, fold_change=1.5)
+        hic.filter(blf, queue=True)
+        hic.filter(eof, queue=True)
+        hic.run_queued_filters()
+        print len(hic.edges)
+
+    # join into one object
+    logging.info("Joining objects")
+    coll = HicWeightMeanVariance(hics, file_name=tmpdir + 'coll.m')
+    zwf = ZeroWeightFilter()
+    logging.info("Filtering zero-weights")
+    coll.filter(zwf)
+
+    values = []
+    current = 0
+    for edge in coll.edges_sorted('var', reverse=True, lazy=True):
+        if np.isnan(edge.var):
+            continue
+
+        current += 1
+        if current >= sample_size:
+            break
+
+        weights = []
+        for i in xrange(0, len(hics)):
+            weights.append(edge['weight_' + str(i)])
+        values.append(weights)
