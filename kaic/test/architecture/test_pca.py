77a42d703f1bf15bead5cc99ec3b54530ed00577
kai <kai.kruse@mpi-muenster.mpg.de>
2016-05-11 09:14:27 +0200

    Test for per_chromosome PCA

--- kaic/test/architecture/test_pca.py
+++ kaic/test/architecture/test_pca.py
@@ -55,7 +55,23 @@ class TestPCA:
 
         # euclidian distances
         def euclid(xy1, xy2):
-            return math.sqrt((xy1[0]-xy2[0])**2+(xy1[1]-xy2[1])**2)
+            return math.sqrt((xy1[0] - xy2[0]) ** 2 + (xy1[1] - xy2[1]) ** 2)
+
+        related_pair_dist = euclid((res[2, 0], res[2, 1]), (res[3, 0], res[3, 1]))
+        pairs = ((0, 1), (0, 2), (0, 3), (1, 2), (1, 3))
+        for pair in pairs:
+            i = pair[0]
+            j = pair[1]
+            assert related_pair_dist < euclid((res[i, 0], res[i, 1]), (res[j, 0], res[j, 1]))
+
+    def test_pca_chromosome(self):
+        hics = [self.hic1, self.hic2, self.hic3, self.hic4]
+
+        pca, res = do_pca(hics, sample_size=25, regions='chr3')
+
+        # euclidian distances
+        def euclid(xy1, xy2):
+            return math.sqrt((xy1[0] - xy2[0]) ** 2 + (xy1[1] - xy2[1]) ** 2)
 
         related_pair_dist = euclid((res[2, 0], res[2, 1]), (res[3, 0], res[3, 1]))
         pairs = ((0, 1), (0, 2), (0, 3), (1, 2), (1, 3))
