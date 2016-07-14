a2aa3fb6c50483a75d85ce0a99695e967d6a57fb
kai <kai.kruse@mpi-muenster.mpg.de>
2016-07-14 17:28:05 +0200

    Necessary registry module for registering and retrieving objects by name/id

---
+++ kaic/data/registry.py
@@ -0,0 +1,16 @@
+class_name_dict = {}
+class_id_dict = {}
+
+
+def get_class_by_name(classname):
+    """
+    Get the node class matching the `classname`.
+    If the name is not registered, a ``TypeError`` is raised.
+    """
+
+    # Get the class object corresponding to `classname`.
+    if classname not in class_name_dict:
+        raise TypeError("there is no registered node class named ``%s``"
+                        % (classname,))
+
+    return class_name_dict[classname]
