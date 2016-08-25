041d1025331a9fd7cc7fd3bb800892f892e6c769
kai <kai.kruse@mpi-muenster.mpg.de>
2016-08-25 11:23:10 +0200

    Minor version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='kaic',
-    version='0.4.0',
+    version='0.4.1',
     description='Hi-C data analysis tools.',
     packages=find_packages(exclude=["test"]),
     install_requires=[
