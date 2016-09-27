cb89832da1caa5df433519d836bb34443f0d4530
kai <kai.kruse@mpi-muenster.mpg.de>
2016-09-27 12:04:07 +0200

    Added Map class for dot-notation access to dict (see http://stackoverflow.com/questions/2352181/how-to-use-a-dot-to-access-members-of-dictionary)

--- kaic/tools/general.py
+++ kaic/tools/general.py
@@ -1,9 +1,3 @@
-'''
-Created on Aug 28, 2015
-
-@author: kkruse1
-'''
-
 import itertools
 import random
 import collections
@@ -12,6 +6,40 @@ import tables as t
 import os
 import errno
 from datetime import datetime
+
+
+class Map(dict):
+    """
+    Example:
+    m = Map({'first_name': 'Foo'}, last_name='Bar', age=99, sports=['Baz'])
+    """
+    def __init__(self, *args, **kwargs):
+        super(Map, self).__init__(*args, **kwargs)
+        for arg in args:
+            if isinstance(arg, dict):
+                for k, v in arg.iteritems():
+                    self[k] = v
+
+        if kwargs:
+            for k, v in kwargs.iteritems():
+                self[k] = v
+
+    def __getattr__(self, attr):
+        return self.get(attr)
+
+    def __setattr__(self, key, value):
+        self.__setitem__(key, value)
+
+    def __setitem__(self, key, value):
+        super(Map, self).__setitem__(key, value)
+        self.__dict__.update({key: value})
+
+    def __delattr__(self, item):
+        self.__delitem__(item)
+
+    def __delitem__(self, key):
+        super(Map, self).__delitem__(key)
+        del self.__dict__[key]
 
 
 class CachedIterator:
