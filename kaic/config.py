e462752d6b2054420d9fa1b62c14dc832ba5ef23
kai <kai.kruse@mpi-muenster.mpg.de>
2016-09-27 15:14:36 +0200

    Implemented superseeding of options by config files with different priorities

--- kaic/config.py
+++ kaic/config.py
@@ -6,17 +6,20 @@ import warnings
 import logging
 logger = logging.getLogger(__name__)
 
-default_config_content = """\
+_default_config_content = """\
 colormap_hic: germany
 """
-default_config = yaml.load(default_config_content)
+default_config = yaml.load(_default_config_content)
 
 _config_file_name = 'kaic.conf'
 _environment_variable_name = 'KAIC_CONF'
 
 # check common places for config file
 # modified from: http://stackoverflow.com/questions/7567642/where-to-put-a-configuration-file-in-python
-locations = (
+# the order of files in this tuple determines config file priority
+# options in files with higher priority override options in files with lower priority
+# default options have lowest priority
+_config_file_locations = (
     # current directory
     os.path.join(os.curdir, _config_file_name),
     # environment variable
@@ -31,32 +34,38 @@ locations = (
     os.path.join("/etc/kaic/", _config_file_name)
 )
 
+
 # read the first config file that is found, checking in the order above
-file_config = None
-for location in locations:
-    if location is None:
-        continue
+def read_file_configs(config_file_locations):
+    configs = []
+    for location in config_file_locations:
+        if location is None:
+            continue
 
-    config_path = os.path.expanduser(location)
-    if file_config is None and os.path.exists(config_path):
-        logging.info("Loading config from {}".format(config_path))
-        try:
-            with open(config_path, 'r') as config_file:
-                config_file_content = config_file.read()
-                file_config = yaml.safe_load(config_file_content)
-        except IOError, e:
-            logging.error("Could not read config file {}".format(config_path), e)
-            pass
+        config_path = os.path.expanduser(location)
+        if os.path.exists(config_path):
+            logging.debug("Loading config from {}".format(config_path))
+            try:
+                with open(config_path, 'r') as config_file:
+                    config_file_content = config_file.read()
+                    file_config = yaml.safe_load(config_file_content)
+                    configs.append((config_path, file_config))
+            except IOError, e:
+                logging.error("Could not read config file {}".format(config_path), e)
+                pass
+    return configs
 
-config_dict = copy.deepcopy(default_config)
-if file_config is not None:
-    for key, value in file_config.iteritems():
+file_configs = read_file_configs(_config_file_locations)
+
+_config_dict = copy.deepcopy(default_config)
+for current_config_path, current_file_config in reversed(file_configs):
+    for key, value in current_file_config.iteritems():
         if key not in default_config:
             warnings.warn("Config option '{}' is not known".format(key))
         else:
-            config_dict[key] = value
+            _config_dict[key] = value
 
-config = Map(config_dict)
+config = Map(_config_dict)
 
 
 def write_default_config(file_name, overwrite=False):
@@ -65,4 +74,4 @@ def write_default_config(file_name, overwrite=False):
         raise IOError("Config file exists in this location ({}). Use overwrite=True or remove file.".format(file_name))
 
     with open(file_name, 'w') as output_config_file:
-        output_config_file.write(default_config_content)
+        output_config_file.write(_default_config_content)
