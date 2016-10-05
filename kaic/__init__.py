145950d6d8b926a34edc9c7464b7fec9b3f594a1
kai <kai.kruse@mpi-muenster.mpg.de>
2016-10-05 10:56:48 +0200

    Alias for long class name

--- kaic/__init__.py
+++ kaic/__init__.py
@@ -16,6 +16,7 @@ from kaic.data.genomic import Hic, Node, Edge, Genome, Chromosome, Bed, AccessOp
 from kaic.data.general import FileBased
 from kaic.data.registry import class_id_dict
 from kaic.construct.seq import Reads, FragmentMappedReadPairs
+from kaic.construct.seq import FragmentMappedReadPairs as Pairs  # alias for FragmentMappedReadPairs
 from kaic.architecture.hic_architecture import DirectionalityIndex, InsulationIndex, PossibleContacts, \
     ExpectedContacts, RegionContactAverage, FoldChangeMatrix, ObservedExpectedRatio, ABDomains, \
     ABDomainMatrix, MetaArray, MetaHeatmap, VectorDifference, VectorArchitecturalRegionFeature, \
