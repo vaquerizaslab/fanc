ee44d815555f294f7abb7e3231f800e96c7232c7
kai <kai.kruse@mpi-muenster.mpg.de>
2017-02-15 15:22:07 +0100

    Corrected circular import
    Former-commit-id: cc3a6a589ce9053af86884bddbe5aa54a81791c5

--- kaic/tools/general.py
+++ kaic/tools/general.py
@@ -5,8 +5,6 @@ import progressbar
 import tables as t
 import os
 import errno
-import signal
-from kaic.config import config
 from builtins import object
 from datetime import datetime
 
@@ -206,29 +204,3 @@ class RareUpdateProgressBar(progressbar.ProgressBar):
         elif self.manual_poll_interval:
             delta = datetime.now() - self.last_update_time
             return delta > self.poll_interval
-
-
-class TrappedSignalException(Exception):
-    pass
-
-
-# trap signals requested by user and throw custom exception
-def _trapped_signal_handler(signum, frame):
-    try:
-        message = "Trapped signal {} ({})".format(signum, signal.Signals(signum).name)
-    except AttributeError:
-        message = "Trapped signal {}".format(signum)
-
-    raise TrappedSignalException(message)
-
-
-def enable_signal_traps(signal_names=config.trap_signals):
-    for signal_name in signal_names:
-        s = getattr(signal, signal_name)
-        signal.signal(s, _trapped_signal_handler)
-
-
-def disable_signal_traps(signal_names=config.trap_signals):
-    for signal_name in signal_names:
-        s = getattr(signal, signal_name)
-        signal.signal(s, signal.SIG_DFL)
