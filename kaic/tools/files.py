fc25a59c2181e804f342b0d2cda99544ac03e73b
kai <kai.kruse@mpi-muenster.mpg.de>
2017-07-28 14:13:50 +0200

    Fix filename not returned by shutil.copy
    Former-commit-id: c49b22645ba32a905b76aeda710b7e1f8e09c363

--- kaic/tools/files.py
+++ kaic/tools/files.py
@@ -316,7 +316,6 @@ def split_sam(sam_file, output_folder, chunk_size=5000000):
 
 
 def merge_sam(input_sams, output_sam, tmp=None):
-    logger.info(input_sams)
     output_sam = os.path.expanduser(output_sam)
     first_sam = os.path.expanduser(input_sams[0])
 
@@ -334,11 +333,11 @@ def merge_sam(input_sams, output_sam, tmp=None):
             mode = 'wb' if os.path.splitext(output_sam)[1] == '.bam' else 'wh'
             with pysam.AlignmentFile(output_sam, mode, template=fs) as o:
                 for input_sam in input_sams:
-                    logger.info(input_sam)
                     input_sam = os.path.expanduser(input_sam)
                     if tmp:
-                        input_sam = shutil.copy(input_sam, tmp)
-                    logger.info(input_sam)
+                        shutil.copy(input_sam, tmp)
+                        input_sam = os.path.join(tmp, os.path.basename(input_sam))
+                        logger.info("Copied input SAM to {}".format(input_sam))
                     with pysam.AlignmentFile(input_sam) as r:
                         for alignment in r:
                             o.write(alignment)
