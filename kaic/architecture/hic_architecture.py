b333c8828a72ab6e4fa96cb4a4e762c3d13c76e0
kai <kai.kruse@mpi-muenster.mpg.de>
2017-10-09 16:52:06 +0200

    * TAD strength analysis * loop strength analysis
    Former-commit-id: 6655b2ecfce0b2b103809714c6419e1af50ef59b

--- kaic/architecture/hic_architecture.py
+++ kaic/architecture/hic_architecture.py
@@ -2329,23 +2329,28 @@ def _aggregate_region_bins(hic, region, offset=0):
 
 
 def extract_submatrices(hic, region_pairs, norm=False,
-                        log=True, cache=True, mask=True, mask_inf=True):
+                        log=True, cache=True, mask=True, mask_inf=True,
+                        keep_invalid=False):
     cl = hic.chromosome_lengths
     cb = hic.chromosome_bins
 
-    discarded, total = 0, 0
     valid_region_pairs = defaultdict(list)
-    logger.info("Filtering invalid region pairs")
-    for (region1, region2) in region_pairs:
-        total += 1
+    invalid_region_pairs = list()
+    logger.info("Checking region pair validity...")
+    valid, invalid = 0, 0
+    for ix, (region1, region2) in enumerate(region_pairs):
+        is_invalid = False
         if region1.start < 1 or region1.chromosome not in cl or region1.end > cl[region1.chromosome]:
-            discarded += 1
-            continue
+            is_invalid = True
         if region2.start < 1 or region2.chromosome not in cl or region2.end > cl[region2.chromosome]:
-            discarded += 1
-            continue
-        valid_region_pairs[(region1.chromosome, region2.chromosome)].append((region1, region2))
-    logger.info("Filtered {}/{} region pairs".format(discarded, total))
+            is_invalid = True
+        if is_invalid:
+            invalid += 1
+            invalid_region_pairs.append(ix)
+        else:
+            valid += 1
+            valid_region_pairs[(region1.chromosome, region2.chromosome)].append((ix, region1, region2))
+    logger.info("{}/{} region pairs are invalid".format(invalid, valid))
 
     intra_expected = dict()
     inter_expected = None
@@ -2353,7 +2358,9 @@ def extract_submatrices(hic, region_pairs, norm=False,
     logger.info("Calculating mappability...")
     mappable = hic.mappable()
 
-    with RareUpdateProgressBar(max_value=total-discarded, prefix='Matrices') as pb:
+    order = []
+    matrices = []
+    with RareUpdateProgressBar(max_value=valid, prefix='Matrices') as pb:
         current_matrix = 0
         for (chromosome1, chromosome2), regions_pairs_by_chromosome in valid_region_pairs.items():
             if cache:
@@ -2366,7 +2373,7 @@ def extract_submatrices(hic, region_pairs, norm=False,
                 offset1 = 0
                 offset2 = 0
 
-            for (region1, region2) in regions_pairs_by_chromosome:
+            for (region_ix, region1, region2) in regions_pairs_by_chromosome:
                 current_matrix += 1
                 region1_bins = _aggregate_region_bins(hic, region1, offset1)
                 region2_bins = _aggregate_region_bins(hic, region2, offset2)
@@ -2411,7 +2418,15 @@ def extract_submatrices(hic, region_pairs, norm=False,
                     m.mask += m_mask
 
                 pb.update(current_matrix)
-                yield m
+                matrices.append(m)
+                order.append(region_ix)
+
+    if keep_invalid:
+        for region_ix in invalid_region_pairs:
+            matrices.append(None)
+            order.append(region_ix)
+
+    return [matrices[ix] for ix in np.argsort(order)]
 
 
 def _rescale_oe_matrix(matrix, bin_size, scaling_exponent=-0.25):
@@ -2433,6 +2448,10 @@ def _rescale_oe_matrix(matrix, bin_size, scaling_exponent=-0.25):
 def aggregate_boundaries(hic, boundary_regions, window=200000,
                          rescale=False, scaling_exponent=-0.25,
                          **kwargs):
+    kwargs.setdefault('norm', True)
+    kwargs.setdefault('keep_invalid', False)
+    kwargs.setdefault('log', True)
+
     region_pairs = []
     for region in boundary_regions:
         new_start = int(region.center - int(window / 2))
@@ -2464,27 +2483,37 @@ def aggregate_boundaries(hic, boundary_regions, window=200000,
     return am
 
 
-def aggregate_tads(hic, tad_regions, pixels=90, rescale=False, scaling_exponent=-0.25,
-                   interpolation='nearest', keep_mask=True,
-                   absolute_extension=0, relative_extension=1.0,
-                   **kwargs):
-
+def _tad_matrix_iterator(hic, tad_regions, absolute_extension=0, relative_extension=1., **kwargs):
     region_pairs = []
     for region in tad_regions:
         new_region = region.expand(absolute=absolute_extension, relative=relative_extension)
         region_pairs.append((new_region, new_region))
 
+    for m in extract_submatrices(hic, region_pairs, **kwargs):
+        yield m
+
+
+def aggregate_tads(hic, tad_regions, pixels=90, rescale=False, scaling_exponent=-0.25,
+                   interpolation='nearest', keep_mask=True,
+                   absolute_extension=0, relative_extension=1.0,
+                   **kwargs):
+    kwargs.setdefault('norm', True)
+    kwargs.setdefault('keep_invalid', False)
+    kwargs.setdefault('log', True)
+
     shape = (pixels, pixels)
     counter_matrix = np.zeros(shape)
     matrix_sum = np.zeros(shape)
-    for m in extract_submatrices(hic, region_pairs, **kwargs):
+    for m in _tad_matrix_iterator(hic, tad_regions,
+                                  absolute_extension=absolute_extension,
+                                  relative_extension=relative_extension, **kwargs):
         ms = imresize(m, shape, interp=interpolation, mode='F')
 
-        if keep_mask and hasattr(m, 'mask'):
+        if keep_mask and hasattr(ms, 'mask'):
             mask = imresize(m.mask, shape, interp='nearest').astype('bool')
+            ms = np.ma.masked_where(mask, ms)
             inverted_mask = ~mask
             counter_matrix += inverted_mask.astype('int')
-            ms = np.ma.masked_where(mask, ms)
         else:
             counter_matrix += np.ones(shape)
 
@@ -2498,35 +2527,75 @@ def aggregate_tads(hic, tad_regions, pixels=90, rescale=False, scaling_exponent=
     return am
 
 
-def aggregate_loops(hic, loop_regions, pixels=16, **kwargs):
-    left = int(pixels/2)
+def tad_strength(hic, tad_regions, **kwargs):
+    kwargs.setdefault('norm', True)
+    kwargs.setdefault('keep_invalid', False)
+    kwargs.setdefault('log', False)
+    kwargs['relative_extension'] = 1.
+    kwargs['absolute_extension'] = 0
+
+    tad_strengths = []
+    for m in _tad_matrix_iterator(hic, tad_regions, **kwargs):
+        tl = int(m.shape[0]/3)
+        upper_third = slice(0, tl)
+        middle_third = slice(tl, 2*tl)
+        lower_third = slice(2*tl, m.shape[0])
+        tad_sum = m[middle_third, middle_third].sum() / np.logical_not(m.mask)[middle_third, middle_third].sum()
+        upper_sum = m[upper_third, middle_third].sum() / np.logical_not(m.mask)[upper_third, middle_third].sum()
+        lower_sum = m[lower_third, upper_third].sum() / np.logical_not(m.mask)[lower_third, upper_third].sum()
+        ts = float(tad_sum / ((upper_sum + lower_sum) / 2))
+        tad_strengths.append(np.log2(ts))
+    return tad_strengths
+
+
+def _loop_regions_from_bedpe(bedpe):
+    anchors = []
+    for region in bedpe.regions:
+        a1 = GenomicRegion(chromosome=region.chromosome1, start=region.start1, end=region.end1)
+        a2 = GenomicRegion(chromosome=region.chromosome2, start=region.start2, end=region.end2)
+        anchors.append((a1, a2))
+    return anchors
+
+
+def _loop_matrix_iterator(hic, loop_regions, pixels=16, **kwargs):
+    left = int(pixels / 2)
     right = left if pixels % 2 == 1 else left - 1
 
     if isinstance(loop_regions, Bedpe):
-        anchors = []
-        for region in loop_regions.regions:
-            a1 = GenomicRegion(chromosome=region.chromosome1, start=region.start1, end=region.end1)
-            a2 = GenomicRegion(chromosome=region.chromosome2, start=region.start2, end=region.end2)
-            anchors.append((a1, a2))
-        loop_regions = anchors
+        loop_regions = _loop_regions_from_bedpe(loop_regions)
 
     bin_size = hic.bin_size
     region_pairs = []
+    invalid = 0
     for (anchor1, anchor2) in loop_regions:
         a1 = GenomicRegion(chromosome=anchor1.chromosome, start=anchor1.center, end=anchor1.center)
         a2 = GenomicRegion(chromosome=anchor2.chromosome, start=anchor2.center, end=anchor2.center)
-        r1 = list(hic.regions(a1))[0].copy()
-        r2 = list(hic.regions(a2))[0].copy()
-        r1.start -= left * bin_size
-        r1.end += right * bin_size
-        r2.start -= left * bin_size
-        r2.end += right * bin_size
-        region_pairs.append((r1, r2))
+
+        try:
+            r1 = list(hic.regions(a1))[0].copy()
+            r2 = list(hic.regions(a2))[0].copy()
+            r1.start -= left * bin_size
+            r1.end += right * bin_size
+            r2.start -= left * bin_size
+            r2.end += right * bin_size
+            region_pairs.append((r1, r2))
+        except IndexError:
+            invalid += 1
+    logger.warning("{} region pairs invalid, most likely due to missing chromosome data".format(invalid))
+
+    for m in extract_submatrices(hic, region_pairs, **kwargs):
+        yield m
+
+
+def aggregate_loops(hic, loop_regions, pixels=16, **kwargs):
+    kwargs.setdefault('norm', True)
+    kwargs.setdefault('keep_invalid', False)
+    kwargs.setdefault('log', True)
 
     shape = (pixels, pixels)
     counter_matrix = np.zeros(shape)
     matrix_sum = np.zeros(shape)
-    for m in extract_submatrices(hic, region_pairs, **kwargs):
+    for m in _loop_matrix_iterator(hic, loop_regions, pixels=pixels, **kwargs):
         if hasattr(m, 'mask'):
             inverted_mask = ~m.mask
             counter_matrix += inverted_mask.astype('int')
@@ -2535,6 +2604,55 @@ def aggregate_loops(hic, loop_regions, pixels=16, **kwargs):
         matrix_sum += m
 
     return matrix_sum/counter_matrix
+
+
+def loop_strength(hic, loop_regions, pixels=16, **kwargs):
+    kwargs.setdefault('log', False)
+    kwargs.setdefault('norm', True)
+    kwargs['keep_invalid'] = True
+
+    if isinstance(loop_regions, Bedpe):
+        loop_regions = _loop_regions_from_bedpe(loop_regions)
+
+    # generating new regions
+    new_region_pairs = []  # 0: original, 1: control left, 2: control right
+    for (region1, region2) in loop_regions:
+        d = int(abs(region1.center - region2.center))
+        new_left = GenomicRegion(chromosome=region1.chromosome, start=region1.start - d, end=region1.end - d)
+        new_right = GenomicRegion(chromosome=region1.chromosome, start=region2.start + d, end=region2.end + d)
+        new_region_pairs.append((region1, region2))
+        new_region_pairs.append((new_left, region1))
+        new_region_pairs.append((region2, new_right))
+
+    original, left, right = [], [], []
+    for i, m in enumerate(_loop_matrix_iterator(hic, new_region_pairs, pixels=pixels, **kwargs)):
+        if m is not None:
+            value = float(m.sum()/np.logical_not(m.mask).sum())
+        else:
+            value = None
+
+        if i % 3 == 0:
+            original.append(value)
+        elif i % 3 == 1:
+            left.append(value)
+        else:
+            right.append(value)
+
+    ratios = []
+    for i in range(len(original)):
+        if original[i] is None:
+            continue
+        if left[i] is None and right[i] is None:
+            continue
+
+        if left[i] is None:
+            r = original[i]/right[i]
+        elif right[i] is None:
+            r = original[i]/left[i]
+        else:
+            r = original[i]/((left[i]+right[i])/2)
+        ratios.append(np.log2(r))
+    return ratios
 
 
 def ab_enrichment_profile(hic, genome, percentiles=(20.0, 40.0, 60.0, 80.0, 100.0),
@@ -2623,6 +2741,11 @@ def ab_enrichment_profile(hic, genome, percentiles=(20.0, 40.0, 60.0, 80.0, 100.
 
     m /= c
     return np.log2(m)
+
+
+"""
+Filters for architecture objects
+"""
 
 
 class ZeroWeightFilter(MatrixArchitecturalRegionFeatureFilter):
