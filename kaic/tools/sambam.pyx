ea5aa9c2b8b572037bdc0b7d57bfe7f3b4dfaca6
kai <kai.kruse@mpi-muenster.mpg.de>
2018-02-09 10:06:24 +0100

    Ignore read pair suffix when comparing qnames
    Former-commit-id: a2a01c88e6e6c545c2ddcb96a6a2b790d453e2c9

--- kaic/tools/sambam.pyx
+++ kaic/tools/sambam.pyx
@@ -1,7 +1,13 @@
 cdef extern from  "natural_cmp.c":
     int strnum_cmp(const char *_a, const char *_b)
 
+slash_int = b'/'[0]
+
 def natural_cmp(a, b):
     assert (isinstance(a, bytes))
     assert (isinstance(b, bytes))
-    return strnum_cmp(a, b)
+
+    if a[-2] == b[-2] == slash_int:
+        return strnum_cmp(a[:-2], b[:-2])
+    else:
+        return strnum_cmp(a, b)
