2e90450555ab6a0a32e0cdf5e0f77eec79d642c2
kai <kai.kruse@mpi-muenster.mpg.de>
2018-12-27 08:54:21 +0100

    comment refactored modules
    Former-commit-id: 6ade4a38dcd3494dbfd249a875c527acc59ff27d

--- kaic/__init__.py
+++ kaic/__init__.py
@@ -10,19 +10,19 @@ Provides
 """
 from .version import __version__
 
-from kaic.config import config
-from genomic_regions import GenomicRegion, as_region, load as gr_load
-from kaic.data.genomic import Hic, Node, Edge, Genome, Chromosome, AccessOptimisedHic, load_hic
-from kaic.data.general import FileBased
-from kaic.data.registry import class_id_dict
-from kaic.construct.seq import Reads, FragmentMappedReadPairs
-from kaic.construct.seq import AccessOptimisedReadPairs as Pairs  # alias
-from kaic.architecture.hic_architecture import DirectionalityIndex, InsulationIndex, PossibleContacts, \
-    ExpectedContacts, RegionContactAverage, FoldChangeMatrix, ObservedExpectedRatio, ABDomains, \
-    ABDomainMatrix, MetaArray, MetaHeatmap, VectorDifference, VectorArchitecturalRegionFeature, \
-    MultiVectorArchitecturalRegionFeature, cumulative_matrix
-from kaic.data.network import RaoPeakInfo
-from kaic.architecture.genome_architecture import GenomicTrack
+# from kaic.config import config
+# from genomic_regions import GenomicRegion, as_region, load as gr_load
+# from kaic.data.genomic import Hic, Node, Edge, Genome, Chromosome, AccessOptimisedHic, load_hic
+# from kaic.data.general import FileBased
+# from kaic.data.registry import class_id_dict
+# from kaic.construct.seq import Reads, FragmentMappedReadPairs
+# from kaic.construct.seq import AccessOptimisedReadPairs as Pairs  # alias
+# from kaic.architecture.hic_architecture import DirectionalityIndex, InsulationIndex, PossibleContacts, \
+#     ExpectedContacts, RegionContactAverage, FoldChangeMatrix, ObservedExpectedRatio, ABDomains, \
+#     ABDomainMatrix, MetaArray, MetaHeatmap, VectorDifference, VectorArchitecturalRegionFeature, \
+#     MultiVectorArchitecturalRegionFeature, cumulative_matrix
+# from kaic.data.network import RaoPeakInfo
+# from kaic.architecture.genome_architecture import GenomicTrack
 import tables
 import os
 import logging
@@ -32,77 +32,77 @@ logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
 
 
-def load(file_name, *args, **kwargs):
-    import os
-    file_name = os.path.expanduser(file_name)
-    try:
-        f = FileBased(file_name, mode='r')
-        mode = kwargs.pop('mode', 'r')
-        classid = None
-        try:
-            classid = f.meta._classid
-            classid = classid.decode() if isinstance(classid, bytes) else classid
-            f.close()
-            cls_ = class_id_dict[classid]
-            logger.debug("Detected {}".format(cls_))
-            return cls_(file_name=file_name, mode=mode, *args, **kwargs)
-        except AttributeError:
-            # try to detect from file structure
-
-            # Hi-C
-            try:
-                n = f.file.get_node('/edges')
-                from kaic.data.general import MaskedTable
-                hic_class = None
-                if isinstance(n, MaskedTable):
-                    hic_class = Hic
-                elif isinstance(n, tables.group.Group):
-                    hic_class = AccessOptimisedHic
-
-                if hic_class is not None:
-                    f.close()
-                    return hic_class(file_name, mode=mode, *args, **kwargs)
-            except tables.NoSuchNodeError:
-                pass
-
-            # others
-            detectables = (
-                ('insulation_index', InsulationIndex),
-                ('directionality_index', DirectionalityIndex),
-                ('contact_average', RegionContactAverage),
-                ('expected_contacts', FoldChangeMatrix),
-                ('distance_decay', ExpectedContacts),
-                ('observed_expected', ObservedExpectedRatio),
-                ('ab_domains', ABDomains),
-                ('ab_domain_matrix', ABDomainMatrix),
-                ('possible_contacts', PossibleContacts),
-                ('meta_matrix', MetaArray),
-                ('meta_heatmap', MetaHeatmap),
-                ('tracks', GenomicTrack),
-                ('fragments', FragmentMappedReadPairs),
-                ('reads', Reads),
-                ('vector_diff', VectorDifference),
-                ('region_data', VectorArchitecturalRegionFeature),
-                ('array_region_data', MultiVectorArchitecturalRegionFeature),
-            )
-
-            for name, cls in detectables:
-                try:
-                    f.file.get_node('/' + name)
-                    f.close()
-                    return cls(file_name, mode=mode, *args, **kwargs)
-                except tables.NoSuchNodeError:
-                    pass
-
-            f.close()
-            raise ValueError("File ({}) does not have a '_classid' meta attribute. This might be fixed by loading the "
-                             "class once explicitly with the appropriate class in append mode. "
-                             "It was also impossible to auto-detect the file type from the file "
-                             "structure.".format(file_name))
-        except KeyError:
-            raise ValueError("classid attribute ({}) does not have a registered class.".format(classid))
-    except tables.HDF5ExtError:
-        return gr_load(file_name, *args, **kwargs)
+# def load(file_name, *args, **kwargs):
+#     import os
+#     file_name = os.path.expanduser(file_name)
+#     try:
+#         f = FileBased(file_name, mode='r')
+#         mode = kwargs.pop('mode', 'r')
+#         classid = None
+#         try:
+#             classid = f.meta._classid
+#             classid = classid.decode() if isinstance(classid, bytes) else classid
+#             f.close()
+#             cls_ = class_id_dict[classid]
+#             logger.debug("Detected {}".format(cls_))
+#             return cls_(file_name=file_name, mode=mode, *args, **kwargs)
+#         except AttributeError:
+#             # try to detect from file structure
+#
+#             # Hi-C
+#             try:
+#                 n = f.file.get_node('/edges')
+#                 from kaic.data.general import MaskedTable
+#                 hic_class = None
+#                 if isinstance(n, MaskedTable):
+#                     hic_class = Hic
+#                 elif isinstance(n, tables.group.Group):
+#                     hic_class = AccessOptimisedHic
+#
+#                 if hic_class is not None:
+#                     f.close()
+#                     return hic_class(file_name, mode=mode, *args, **kwargs)
+#             except tables.NoSuchNodeError:
+#                 pass
+#
+#             # others
+#             detectables = (
+#                 ('insulation_index', InsulationIndex),
+#                 ('directionality_index', DirectionalityIndex),
+#                 ('contact_average', RegionContactAverage),
+#                 ('expected_contacts', FoldChangeMatrix),
+#                 ('distance_decay', ExpectedContacts),
+#                 ('observed_expected', ObservedExpectedRatio),
+#                 ('ab_domains', ABDomains),
+#                 ('ab_domain_matrix', ABDomainMatrix),
+#                 ('possible_contacts', PossibleContacts),
+#                 ('meta_matrix', MetaArray),
+#                 ('meta_heatmap', MetaHeatmap),
+#                 ('tracks', GenomicTrack),
+#                 ('fragments', FragmentMappedReadPairs),
+#                 ('reads', Reads),
+#                 ('vector_diff', VectorDifference),
+#                 ('region_data', VectorArchitecturalRegionFeature),
+#                 ('array_region_data', MultiVectorArchitecturalRegionFeature),
+#             )
+#
+#             for name, cls in detectables:
+#                 try:
+#                     f.file.get_node('/' + name)
+#                     f.close()
+#                     return cls(file_name, mode=mode, *args, **kwargs)
+#                 except tables.NoSuchNodeError:
+#                     pass
+#
+#             f.close()
+#             raise ValueError("File ({}) does not have a '_classid' meta attribute. This might be fixed by loading the "
+#                              "class once explicitly with the appropriate class in append mode. "
+#                              "It was also impossible to auto-detect the file type from the file "
+#                              "structure.".format(file_name))
+#         except KeyError:
+#             raise ValueError("classid attribute ({}) does not have a registered class.".format(classid))
+#     except tables.HDF5ExtError:
+#         return gr_load(file_name, *args, **kwargs)
 
 
 example_data = dict(
