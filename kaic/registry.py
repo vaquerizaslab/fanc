6163f13284171e6cfe72f7715944c88b52696d21
kai <kai.kruse@mpi-muenster.mpg.de>
2019-01-23 11:59:22 +0100

    Update logging statement
    Former-commit-id: 0e68f1f7854e978305adaaae9fc0b6cdd664ecaf

--- kaic/registry.py
+++ kaic/registry.py
@@ -10,7 +10,6 @@ def get_class_by_name(classname):
 
     # Get the class object corresponding to `classname`.
     if classname not in class_name_dict:
-        raise TypeError("there is no registered node class named ``%s``"
-                        % (classname,))
+        raise TypeError("there is no registered node class named ``{}``".format(classname))
 
     return class_name_dict[classname]
