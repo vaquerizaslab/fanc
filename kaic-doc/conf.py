d2fcf1e3300b9367012c4632543c46646feab06a
kai <kai.kruse@mpi-muenster.mpg.de>
2019-03-11 09:24:12 +0100

    Fix copyright note

--- kaic-doc/conf.py
+++ kaic-doc/conf.py
@@ -56,7 +56,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Kai-C'
-copyright = u'2016, Kai Kruse'
+copyright = u'2019, Kai Kruse, Vaquerizas lab'
 author = u'Kai Kruse'
 
 # The version info for the project you're documenting, acts as replacement for
