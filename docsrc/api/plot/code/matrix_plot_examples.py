3d55dd15a967499790fcb87c71730c349023c201
Kai Kruse <kai.kruse@mpi-muenster.mpg.de>
2020-02-05 14:34:12 +0100

    Extended matrix plot API section

--- docsrc/api/plot/code/matrix_plot_examples.py
+++ docsrc/api/plot/code/matrix_plot_examples.py
@@ -35,3 +35,81 @@ hp.show()
 # end snippet fancplot plt axes
 
 hp.save(os.path.join(fanc_base, 'docsrc/api/plot/images/plot_triangular_plt.png'), figsize=(6, 3))
+
+
+# start snippet fancplot square string
+hp = fancplot.SquareMatrixPlot(hic)
+hp.plot('chr18:6mb-10mb')
+hp.show()
+# end snippet fancplot square string
+
+hp.save(os.path.join(fanc_base, 'docsrc/api/plot/images/plot_square.png'), figsize=(6, 6))
+
+
+# start snippet fancplot square vmax
+hp = fancplot.SquareMatrixPlot(hic, vmax=0.05)
+hp.plot('chr18:6mb-10mb')
+hp.show()
+# end snippet fancplot square vmax
+
+hp.save(os.path.join(fanc_base, 'docsrc/api/plot/images/plot_square_vmax.png'), figsize=(6, 6))
+
+
+# start snippet fancplot square cmap
+hp = fancplot.SquareMatrixPlot(hic, vmax=0.05, colormap='white_red')
+hp.plot('chr18:6mb-10mb')
+hp.show()
+# end snippet fancplot square cmap
+
+hp.save(os.path.join(fanc_base, 'docsrc/api/plot/images/plot_square_cmap.png'), figsize=(6, 6))
+
+
+# start snippet fancplot square log
+hp = fancplot.SquareMatrixPlot(hic, vmax=0.05, norm='log')
+hp.plot('chr18:6mb-10mb')
+hp.show()
+# end snippet fancplot square log
+
+hp.save(os.path.join(fanc_base, 'docsrc/api/plot/images/plot_square_log.png'), figsize=(6, 6))
+
+# start snippet fancplot square uncorrected
+hp = fancplot.SquareMatrixPlot(hic, vmax=30, matrix_norm=False)
+hp.plot('chr18:6mb-10mb')
+hp.show()
+# end snippet fancplot square uncorrected
+
+hp.save(os.path.join(fanc_base, 'docsrc/api/plot/images/plot_square_uncorrected.png'), figsize=(6, 6))
+
+
+# start snippet fancplot square oe
+hp = fancplot.SquareMatrixPlot(hic, vmin=-2, vmax=2, oe=True, log=True,
+                               colormap='RdBu_r')
+hp.plot('chr18:6mb-10mb')
+hp.show()
+# end snippet fancplot square oe
+
+hp.save(os.path.join(fanc_base, 'docsrc/api/plot/images/plot_square_oe.png'), figsize=(6, 6))
+
+
+fig, ax = plt.subplots(figsize=(6, 3))
+# start snippet fancplot triangular maxdist
+hp = fancplot.TriangularMatrixPlot(hic, vmax=0.05, max_dist='2mb')
+hp.plot('chr18:6mb-10mb')
+hp.show()
+# end snippet fancplot triangular maxdist
+
+hp.save(os.path.join(fanc_base, 'docsrc/api/plot/images/plot_triangular_maxdist.png'), figsize=(6, 3))
+
+
+fig, ax = plt.subplots(figsize=(10, 10))
+# start snippet fancplot mirror
+# first create two triangular plots
+top_plot = fancplot.TriangularMatrixPlot(hic, vmax=0.05, max_dist='2mb')
+bottom_plot = fancplot.TriangularMatrixPlot(hic, vmin=-2, vmax=2, oe=True, log=True, colormap='RdBu_r', max_dist='2mb')
+# then merge them
+hp = fancplot.MirrorMatrixPlot(top_plot, bottom_plot)
+hp.plot('chr18:6mb-10mb')
+hp.show()
+# end snippet fancplot mirror
+
+hp.save(os.path.join(fanc_base, 'docsrc/api/plot/images/plot_mirror.png'))
